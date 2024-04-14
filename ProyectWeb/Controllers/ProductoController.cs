a54b0792d529290fa202efe0d701938964d553ed
Andrea Santos <andikarosagas@outlook.com>
2024-04-14 09:16:00 -0600

    Agregue el Controlador de Producto

---
+++ ProyectWeb/Controllers/ProductoController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectWeb.DataBase;
+using ProyectWeb.Models;
+
+namespace ProyectWeb.Controllers
+{
+    public class ProductoController : Controller
+    {
+        private ProyectoContext db = new ProyectoContext();
+
+        public ActionResult Index()
+        {
+            var producto = db.Producto.Include(p => p.Categoria).Include(p => p.UnidadMedida);
+            return View(producto.ToList());
+        }
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Producto producto = db.Producto.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producto);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.CategoriaId = new SelectList(db.Categoria, "CategoriaId", "Codigo");
+            ViewBag.UnidadMedidaId = new SelectList(db.UnidadMedida, "UnidadMedidaId", "Codigo");
+            return View(new Producto());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductoId,CategoriaId,UnidadMedidaId,FechaCreacion,Estado,PrecioCompra")] Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Producto.Add(producto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoriaId = new SelectList(db.Categoria, "CategoriaId", "Codigo", producto.CategoriaId);
+            ViewBag.UnidadMedidaId = new SelectList(db.UnidadMedida, "UnidadMedidaId", "Codigo", producto.UnidadMedidaId);
+            return View(producto);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Producto producto = db.Producto.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoriaId = new SelectList(db.Categoria, "CategoriaId", "Codigo", producto.CategoriaId);
+            ViewBag.UnidadMedidaId = new SelectList(db.UnidadMedida, "UnidadMedidaId", "Codigo", producto.UnidadMedidaId);
+            return View(producto);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductoId,CategoriaId,UnidadMedidaId,FechaCreacion,Estado,PrecioCompra")] Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(producto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoriaId = new SelectList(db.Categoria, "CategoriaId", "Codigo", producto.CategoriaId);
+            ViewBag.UnidadMedidaId = new SelectList(db.UnidadMedida, "UnidadMedidaId", "Codigo", producto.UnidadMedidaId);
+            return View(producto);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Producto producto = db.Producto.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producto);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Producto producto = db.Producto.Find(id);
+            db.Producto.Remove(producto);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
