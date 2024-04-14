dd48933d07639a5afdcc055f1916661df4784e73
Ailin <Ailin@DESKTOP-KMVSAAP>
2024-04-14 09:35:25 -0600

    Agregue Controlador de Factura

---
+++ ProyectWeb/Controllers/FacturaController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectWeb.DataBase;
+using ProyectWeb.Models;
+
+namespace ProyectWeb.Controllers
+{
+    public class FacturaController : Controller
+    {
+        private ProyectoContext db = new ProyectoContext();
+        public ActionResult Index()
+        {
+            var factura = db.Factura.Include(f => f.Cliente).Include(f => f.Pedido);
+            return View(factura.ToList());
+        }
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Factura factura = db.Factura.Find(id);
+            if (factura == null)
+            {
+                return HttpNotFound();
+            }
+            return View(factura);
+        }
+        public ActionResult Create()
+        {
+            ViewBag.ClienteId = new SelectList(db.Cliente, "ClienteId", "Codigo");
+            ViewBag.PedidoId = new SelectList(db.Pedido, "PedidoId", "PedidoId");
+            return View(new Factura());
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "FacturaId,ClienteId,PedidoId,FechaCreacion,FechaFactura,Estado,Total,SubTotal,Descuento")] Factura factura)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Factura.Add(factura);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ClienteId = new SelectList(db.Cliente, "ClienteId", "Codigo", factura.ClienteId);
+            ViewBag.PedidoId = new SelectList(db.Pedido, "PedidoId", "PedidoId", factura.PedidoId);
+            return View(factura);
+        }
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Factura factura = db.Factura.Find(id);
+            if (factura == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ClienteId = new SelectList(db.Cliente, "ClienteId", "Codigo", factura.ClienteId);
+            ViewBag.PedidoId = new SelectList(db.Pedido, "PedidoId", "PedidoId", factura.PedidoId);
+            return View(factura);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "FacturaId,ClienteId,PedidoId,FechaCreacion,FechaFactura,Estado,Total,SubTotal,Descuento")] Factura factura)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(factura).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ClienteId = new SelectList(db.Cliente, "ClienteId", "Codigo", factura.ClienteId);
+            ViewBag.PedidoId = new SelectList(db.Pedido, "PedidoId", "PedidoId", factura.PedidoId);
+            return View(factura);
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Factura factura = db.Factura.Find(id);
+            if (factura == null)
+            {
+                return HttpNotFound();
+            }
+            return View(factura);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Factura factura = db.Factura.Find(id);
+            db.Factura.Remove(factura);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
