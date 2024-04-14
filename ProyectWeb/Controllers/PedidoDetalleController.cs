3d8bc0af97c42223dd194a2aa712ba842f0ffa34
Ailin <Ailin@DESKTOP-KMVSAAP>
2024-04-14 09:31:38 -0600

    Agregue Controlador de PedidoDetalle

---
+++ ProyectWeb/Controllers/PedidoDetalleController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectWeb.DataBase;
+using ProyectWeb.Models;
+
+namespace ProyectWeb.Controllers
+{
+    public class PedidoDetalleController : Controller
+    {
+        private ProyectoContext db = new ProyectoContext();
+        public ActionResult Index()
+        {
+            var pedido = db.Pedido.Include(p => p.Cliente);
+            return View(pedido.ToList());
+        }
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pedido pedido = db.Pedido.Find(id);
+            if (pedido == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pedido);
+        }
+        public ActionResult Create()
+        {
+            ViewBag.ClienteId = new SelectList(db.Cliente, "ClienteId", "Codigo");
+            return View(new PedidoDetalle());
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PedidoId,ClienteId,FechaCreacion,FechaPedido,Estado,Total,SubTotal,Descuento")] Pedido pedido)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pedido.Add(pedido);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ClienteId = new SelectList(db.Cliente, "ClienteId", "Codigo", pedido.ClienteId);
+            return View(pedido);
+        }
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pedido pedido = db.Pedido.Find(id);
+            if (pedido == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ClienteId = new SelectList(db.Cliente, "ClienteId", "Codigo", pedido.ClienteId);
+            return View(pedido);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PedidoId,ClienteId,FechaCreacion,FechaPedido,Estado,Total,SubTotal,Descuento")] Pedido pedido)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pedido).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ClienteId = new SelectList(db.Cliente, "ClienteId", "Codigo", pedido.ClienteId);
+            return View(pedido);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pedido pedido = db.Pedido.Find(id);
+            if (pedido == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pedido);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Pedido pedido = db.Pedido.Find(id);
+            db.Pedido.Remove(pedido);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
